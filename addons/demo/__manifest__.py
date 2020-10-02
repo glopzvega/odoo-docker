bee4fa45ff4c69756de2636af5f1b67d9084510f
glvega <mcgalv@gmail.com>
2020-10-02 08:30:57 -0500

    Imagen Odoo Docker

---
+++ addons/demo/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Demo Docker",
+
+    'summary': """
+        Demo Docker""",
+
+    'description': """
+        Extra Module Docker
+    """,
+
+    'author': "Gerardo A Lopez Vega",
+    'website': "https://yuju.io/",
+    'category': 'Sales',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base'        
+    ],
+    # always loaded
+    'data': [
+        # 'security/security.xml',
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+}
